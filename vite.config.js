a282fd40857a0f7681c3c6cf1498ba50ce93dc9f
azerkazimov <648azer@gmail.com>
2024-06-22 12:55:28 +0400

    adjust

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -13,7 +12,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/style/sass/style.scss";`,
+        additionalData: `@import "@/styles/sass/style.scss";`,
       },
     },
   },
