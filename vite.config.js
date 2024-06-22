aac73349e68fd1166aea9d599c608b01859ce13e
azerkazimov <648azer@gmail.com>
2024-06-22 11:39:11 +0400

    config styles

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/sass/style.scss";`,
+        additionalData: `@import "@/style/sass/style.scss";`,
       },
     },
   },
