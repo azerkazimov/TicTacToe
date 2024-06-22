b7bfb737c487a4ac227d0dc2a8c7122a190420d4
azerkazimov <648azer@gmail.com>
2024-06-22 15:44:45 +0400

    Tic Tac Toe

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import TicTacToe from "./components/TicTacToe";
+import TicTacToe from "./components/TicTacToe"
 
 function App() {
+
   return (
     <>
-      <TicTacToe />
+      <TicTacToe/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
